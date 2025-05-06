e1ffda4e1eb8b5e87ac7da9bf878b541ad2d3a79
Ciaran Moynihan <ciaran.moynihan@mac.com>
2025-05-06 13:32:05 -0700

    Added First Test

---
+++ test/MonthlyPaymentTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonthlyPaymentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setEnabled() {
+    }
+
+    @Test
+    void getEnabled() {
+    }
+
+    @Test
+    void getPayName() {
+    }
+
+    @Test
+    void setPayName() {
+    }
+
+    @Test
+    void getPriority() {
+    }
+
+    @Test
+    void setPriority() {
+    }
+
+    @Test
+    void getPayTime() {
+    }
+
+    @Test
+    void setPayTime() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void setAmount() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getMonthlyPayment() {
+    }
+}
